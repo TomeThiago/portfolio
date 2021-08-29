6e1d43ae13eab26b28bbbda517c6d7286ceb8372
Guru <g.r.pawar000@gmail.com>
2021-08-29 17:13:50 +0530

    🐛

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 // // ---------- Mobile Menu
 const hamburgerIcon = document.querySelector(".hamburgerIcon");
 const navMenu = document.querySelector("menu");
-const menuListItem = document.querySelectorAll("menu-list-item");
 
 hamburgerIcon.addEventListener("click", mobileMenu);
-
-menuListItem.forEach(ele => closeFunction)
 
 function mobileMenu() {
     hamburgerIcon.classList.toggle("active");
@@ -15,4 +12,3 @@ function mobileMenu() {
 function closeFunction(){ 
     navMenu.classList.toggle("active")
 }
-
